1a412898681bbba112c7c4d205327cd3248a24d8
mark1171 <melvinmendozapowder4@gmail.com>
2026-08-19 00:29:46 +0800

    Update jsonbin-config.js

--- jsonbin-config.js
+++ jsonbin-config.js
@@ -1,10 +1,3 @@
-/* Paste your jsonbin.io details here.
-   1. Sign up free at https://jsonbin.io
-   2. Dashboard -> Create Bin -> paste: {"memories": true} -> Create
-      Copy that bin's ID from the URL/dashboard -> paste below as MEMORIES_ID
-   3. Create a second bin -> paste: {"gallery": true} -> Create
-      Copy its ID -> paste below as GALLERY_ID
-   4. Dashboard -> your profile/API Keys -> copy the X-Master-Key -> paste below */
-window.JSONBIN_MEMORIES_ID = "PASTE_YOUR_MEMORIES_BIN_ID";
-window.JSONBIN_GALLERY_ID = "PASTE_YOUR_GALLERY_BIN_ID";
-window.JSONBIN_API_KEY = "PASTE_YOUR_MASTER_KEY";
+window.JSONBIN_MEMORIES_ID = "6a847bc3f5f4af5e2924ea21";
+window.JSONBIN_GALLERY_ID = "6a847bffda38895dfef359d0";
+window.JSONBIN_API_KEY = "$2a$10$FmttjHCwJjWmdpypD5r.R.0UFlexHIETqnK73/j2huwbusuNHeZpK";
